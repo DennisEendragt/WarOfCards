48092fa65ea5166b6acf48d3acd24242c68a35d8
dsujeeun <dsujeeun@gmail.com>
2017-08-14 12:57:16 -0400

    Created enums for rank and suit of a card

--- Card.java
+++ Card.java
@@ -1,46 +1,43 @@
 package com.dharmeshsujeeun.warofcards;
 
 class Card implements Comparable<Card> {
-    private int rank;
-    private String suit;
+    private Rank rank;
+    private Suit suit;
 
-    public int getRank() {
+    public enum Rank {
+        ACE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6),
+        SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12),
+        KING(13);
+
+        private final int value;
+
+        private Rank(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+    }
+
+    public enum Suit {
+        HEARTS, DIAMONDS, SPADES, CLUBS
+    }
+
+    public Rank getRank() {
         return rank;
     }
 
-    public Card(int rank, String suit) {
+    public Card(Rank rank, Suit suit) {
         this.rank = rank;
         this.suit = suit;
     }
 
     public int compareTo(Card other) {
-        return Integer.compare(rank, other.rank);
+        return Integer.compare(rank.getValue(), other.getRank().getValue());
     }
 
     public String toString() {
-        String representation = "";
-
-        if (rank == 1 || rank > 10) {
-            switch (rank) {
-                case 1:
-                    representation += "Ace";
-                    break;
-                case 11:
-                    representation += "Jack";
-                    break;
-                case 12:
-                    representation += "Queen";
-                    break;
-                case 13:
-                    representation += "King";
-                    break;
-            }
-        } else {
-            representation += rank;
-        }
-
-        representation += " of " + suit;
-
-        return representation;
+        return String.format("%s of %s", rank, suit);
     }
 }
---
+++ CardTest.java
@@ -0,0 +1,8 @@
+package com.dharmeshsujeeun.warofcards;
+
+class CardTest {
+    public static void main(String[] args) {
+        Card queenOfHearts = new Card(Card.Rank.QUEEN, Card.Suit.HEARTS);
+        System.out.println(queenOfHearts);
+    }
+}
